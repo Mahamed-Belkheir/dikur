8010155b1b3705e5a97c68f9a7b2da51f036c271
Mohamed Belkheir <mohamedbel@protonmail.com>
2023-05-09 13:27:20 +0200

    feat(hono): add container parameter to support DI for controllers

--- packages/hono/index.ts
+++ packages/hono/index.ts
@@ -1,26 +1,30 @@
 import { routerMdKey, ParameterInformation, ParameterTypes, RouterNode } from "@dikur/http";
-import { Context, Hono } from "hono"
+import { Context, Hono } from "hono";
 
-export function HonoAdapator(target: new() => any, baseRouter: Hono) {
+type Cls = new () => any;
+
+type Container = (cls: Cls) => InstanceType<Cls>
+
+export function HonoAdapator(target: new() => any, baseRouter: Hono, container: Container = cls => new cls() ) {
     let node = Reflect.getOwnMetadata(routerMdKey, target);
     if (!node) {
         throw new Error(`No routing metadata found in ${target.name}`)
     }
-    return routerMapper(node, baseRouter);
+    return routerMapper(node, baseRouter, container);
 }
 
-function routerMapper(node: RouterNode, baseRouter = new Hono()) {
+function routerMapper(node: RouterNode, baseRouter = new Hono(), container: Container) {
     let r = new Hono()
 
     Object.keys(node.children).forEach(methodName => {
         let details = node.children[methodName];
 
         if ("children" in details) {
-            routerMapper(details, r);
+            routerMapper(details, r, container);
         } else {
             let routeDetails = details;
             r.on(routeDetails.method, routeDetails.path, async (ctx) => {
-                let handler = new node.class();
+                let handler = container(node.class);
                 let parameters = await Promise.all(routeDetails.params.sort((a,b) => a.index - b.index).map(p => honoParamMapper(p, ctx)));
                 let result = await handler[methodName](...parameters);
                 return result;
